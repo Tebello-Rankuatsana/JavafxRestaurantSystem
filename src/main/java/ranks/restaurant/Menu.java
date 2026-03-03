8d56fbc00bddd0e8a94bacfe843f8661e7e3795b
Tebello Rankuatsana <justranku@gmail.com>
2026-03-03 20:28:51 +0200

    changes

---
+++ src/main/java/ranks/restaurant/Menu.java
@@ -0,0 +1,37 @@
+package ranks.restaurant;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Menu {
+
+    @FXML
+    private Button btnChange;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Button btnTotalAmount;
+
+    @FXML
+    private TextField lblAmountTendered;
+
+    @FXML
+    private TextField lblChange;
+
+    @FXML
+    private TextField lblTotalAmount;
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+
+    }
+
+}
