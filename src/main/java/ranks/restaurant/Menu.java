99e038b2855a954651366320adf04e83a1ed7035
Tebello Rankuatsana <justranku@gmail.com>
2026-03-03 22:45:58 +0200

    assigning id numbers to the checkboxes and radio buttons

--- src/main/java/ranks/restaurant/Menu.java
+++ src/main/java/ranks/restaurant/Menu.java
@@ -3,13 +3,33 @@ package ranks.restaurant;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class Menu {
 
     @FXML
+    private CheckBox CheckBoxBeefBurger;
+
+    @FXML
+    private CheckBox CheckBoxCheeseBurger;
+
+    @FXML
+    private CheckBox CheckBoxChickenBurger;
+
+    @FXML
     private MenuButton DrinkMenuButton;
+
+    @FXML
+    private RadioButton RadioButtonChoco;
+
+    @FXML
+    private RadioButton RadioButtonStrawberry;
+
+    @FXML
+    private RadioButton RadioButtonVanilla;
 
     @FXML
     private Button btnChange;
